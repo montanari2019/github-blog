a4ff2704d70cb77863fcedb1f83337a7deef1127
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-03-29 11:40:36 -0400

    seguind

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ import { Router } from "./@Router";
 import { GlobalStyle } from "./styles/global";
 import { DefaultTheme } from "./styles/themes/DefaultThemes";
 
+
+
+import ReactGA from "react-ga";
+const configValue = "G-RRKZJ678B3";
+ReactGA.initialize(configValue);
+
 export function App() {
   return (
     

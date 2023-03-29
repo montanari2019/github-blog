b4b90ce49b66df7bc6f292e408a2a5850fd26ed9
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-03-29 11:40:00 -0400

    editando algumas configurações teste

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-import ReactGA from "react-ga";
+
 import { BrowserRouter } from "react-router-dom";
 
-const configValue = "G-RRKZJ678B3";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -15,4 +15,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   </React.StrictMode>
 );
 
-ReactGA.initialize(configValue);
